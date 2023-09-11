38ec4131223abf0d4af398e6a4cc20658507595a
Zach <zeckhardt9@gmail.com>
2023-09-10 22:25:01 -0400

    randomization

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
+import random
+import requests
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
 
+# Define API endpoint and headers
+BASE_URL = "https://api.themoviedb.org/3"
+API_KEY = os.getenv("API_TOKEN")
+HEADERS = {
+    "accept": "application/json",
+    "Authorization": f'Bearer {API_KEY}'
+}
 
+def fetch_url(url):
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
 
-# Press the green button in the gutter to run the script.
+def get_random_movie_title():
+    page = random.randrange(500)
+    url = f"{BASE_URL}/movie/popular?language=en-US&page={page}&region=US"
+    response = fetch_url(url)
+    results = response.get('results', [])
+    random_movie = random.choice(results)
+    return random_movie['title']
+
+def get_random_show_title():
+    page = random.randrange(500)
+    url = f"{BASE_URL}/tv/popular?language=en-US&page={page}&region=US"
+    response = fetch_url(url)
+    results = response.get('results', [])
+    random_show = random.choice(results)
+    return random_show['name']
+
+def main():
+    print(get_random_show_title())
+    print(get_random_movie_title())
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    main()
